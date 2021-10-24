af884fc27dd39afc7e4c4de1a93e64997fd91c3a
moosch <mail@moosch.io>
2021-10-24 17:11:33 +1030

    Bad spelling

---
+++ word-occurrence-counter/README.md
@@ -0,0 +1,8 @@
+# Word occurence counter
+
+A simple program that accepts 2 string arguments. The path to a file and the word you wish to count the number of occurences of in that file.
+
+```lisp
+(word-occurrence-counter "data.txt" "the")
+69
+```
--- word-occurrence-counter/word-occurrence-counter.lisp
+++ word-occurrence-counter/word-occurrence-counter.lisp
@@ -6,7 +6,7 @@
 
 
 ;; Trying to build my own string splitter
-(defun splitter-by-space (string)
+(defun split-by-space (string)
   (do-split string))
 
 (defun do-split (string &optional (remainder nil))
@@ -17,21 +17,21 @@
 
 
 
-;; reduce though words list and count occurance of input word
+;; reduce though words list and count occurrence of input word
 ;; (reduce #'function-to-apply sequence :initial-value value)
-(defun count-occurences (str word)
+(defun count-occurrences (str word)
   (reduce #'(lambda (acc w)
 	      (if (equal w word)
 		  (+ acc 1)
 		  acc))
-	  (splitter-by-space str) :initial-value 0))
+	  (split-by-space str) :initial-value 0))
 
 
 ;; Putting it all together
-(defun word-occurence-counter (filename word)
+(defun word-occurrence-counter (filename word)
   (progn
     (let ((str (load-in-file filename)))
-      (count-occurences str word))))
+      (count-occurrences str word))))
 
 
 ;; TODO: Then try using streams in case the file size is huge
